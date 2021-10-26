91cecc91468647937301dc9ef1f031162544e95f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-26 07:30:49 -0500

    Initial Commit

---
+++ SwiftUI-OutlineGroup/SwiftUI_OutlineGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OutlineGroupApp.swift
+//  SwiftUI-OutlineGroup
+//
+//  Created by R. Mark Volkmann on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OutlineGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
